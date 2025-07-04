3c4a7b6dda8688bda25f88e4d2180697b60ccf19
Elliot Cooke <elliotcooke@live.com>
2025-07-04 11:31:14 +0100

    Exercise for 1.17 added

--- 1.0 - LearnPython.org/1.17 - List comprehensions.py
+++ 1.0 - LearnPython.org/1.17 - List comprehensions.py
@@ -3,8 +3,18 @@
 #There isn't a keyword to learn for this one, it is a syntax pattern containing
 #square brackets [], a for loop, and an optional if condition
 
-numbers = [1, 5, 6, 74, 4, 456, 2335, 3546, 45657, 2, 2456, 573, 3567, 8645, 2456]
+numbertest = [1, 5, 6, 74, 4, 456, 2335, 3546, 45657, 2, 2456, 573, 3567, 8645, 2456]
 #these are random tehe 
-myFirstListComprehension = [number for number in numbers if number > 2300]
+myFirstListComprehension = [number for number in numbertest if number > 2300]
 #“Make a list of number for each number in numbers if number > 2300.”
+#[expression for variable in iterable if condition]
 print(myFirstListComprehension)
+
+""" Exercise
+Using a list comprehension, create a new list called "newlist" 
+out of the list "numbers", which contains only the positive numbers
+from the list, as integers. """
+#oh, looks like my spin on the first example is actually the exercise
+numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
+newlist = [n for n in numbers if n > 0]
+print(newlist)
