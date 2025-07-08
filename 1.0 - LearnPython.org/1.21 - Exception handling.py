c92fcf570ab15588681ae6f8f3b248655858db26
Elliot Cooke <elliotcooke@live.com>
2025-07-08 22:06:22 +0100

    1.21 Exceptions

---
+++ 1.0 - LearnPython.org/1.21 - Exception handling.py
@@ -0,0 +1,8 @@
+#Python deals with errors by calling them exceptions, e.g
+print(a)
+#Name error: 'a' isn't defined
+
+#obviously a full on crash isn't ideal, so you can add a catch to allow
+#the programme to keep running, even if there is an error
+
+
