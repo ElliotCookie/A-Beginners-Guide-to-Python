a9c6de123b3effc7ca34ad68e5279fad1b21976d
Elliot Cooke <elliotcooke@live.com>
2025-07-14 19:53:29 +0100

    1.26 update and adding 1.27 (decorators)

--- 1.0 - LearnPython.org/1.26 - Closures.py
+++ 1.0 - LearnPython.org/1.26 - Closures.py
@@ -7,24 +7,56 @@ def enclosingFunction(message1):
 	print("This is the enclosing function")
 	def nestedFunction(message2):
 		print("this is the nested function")
-		print(f"{message1} is first and {message2} is second")
+		result = f"{message1} is first and {message2} is second"
+		return result #need to return something or it prints "None"	
 	return nestedFunction
+testMessage = enclosingFunction(input(""))
+print(testMessage(input("")))
 
+#There is something fun you can do with the 'nonlocal' keyword
+def outer(string):
+	def nested():
+		nonlocal string
+		string="hello world"
+		print(string)
+	nested() #Calling it straight away
+	print(string)
+outer("new string that won't print")
 
-testMessage = enclosingFunction(input())
-print(testMessage)
+#You can return the function object rather than calling the nested function, e.g:
+def anotherOuter(anotherVariable):
+	"This is just a line of text?"
+	def anotherNested():
+		"A line of text, but nested"
+		print(anotherVariable)
+	return anotherNested # allows for delaying execution
+function2 = anotherOuter("test time")
+# can do stuff here before calling the function
+function2()
 
-
-
+# Returning the nested allows you to create a reusable stateful function
+# Calling immediately lets you encapsulate logic, but not use it afterwards
 
 
 
 """Exercise
-Make a nested loop and a python closure to make functions to get multiple multiplication
-functions using closures. That is using closures, one could make functions to create
+Make a nested loop and a python closure to make functions
+to get multiple multiplication functions using closures.
+That is using closures, one could make functions to create
 multiply_with_5() or multiply_with_4() functions using closures."""
 
 # your code goes here
+def makeMultiplierFunction(n):
+	def multiplywith(x):
+		result = n * x
+		print(f"{n} x {x} = {result}")
+		return result
+	return multiplywith	
 
-multiplywith5 = multiplier_of(5)
-multiplywith5(9)
+# Ask the user for the multiplier
+mult = int(input("Pick your multiplier: "))
+mult_fn = makeMultiplierFunction(mult)
+
+# Ask for the value to multiply and invoke the closure
+value = int(input("Enter a value to multiply: "))
+mult_fn(value)
---
+++ 1.0 - LearnPython.org/1.27 - Decorators.py
@@ -0,0 +1,13 @@
+# Seems pretty huge, allow you to modify callable objects like classes, methods or funcitons
+# It uses some more keyword-like notation
+
+# Instead of writing
+def function(argument):
+    return "value"
+#function = decorator(function) (<--- errors)
+# passing the function to the decorator, reassigning it
+
+# We can write:
+@decorator
+def anotherFunction(argument):
+    return "value"
