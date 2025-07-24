bab3e165781b7320b8d315f44d264975018867ea
Elliot Cooke <elliotcooke@live.com>
2025-07-24 18:03:29 +0100

    Starting sololearn

---
+++ 2.0 - Sololearn/2.1 - Lesson 1.py
@@ -0,0 +1,8 @@
+print("Welcome")
+print("Level Up!")
+print
+(
+"New email"
+) # didn't know this syntax worked tbh, good to know
+
+#We learnt how to use the pring() function with a quoted string
---
+++ 2.0 - Sololearn/2.2 - Memory & Variables.py
@@ -0,0 +1,6 @@
+country = "France" # I can't be stopped
+
+variable value: "London" # this syntax clealy doesn't actually work
+
+variable name: message
+variable value: "Level Up" # and again
