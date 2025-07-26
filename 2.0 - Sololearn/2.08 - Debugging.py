223139a5494a5ac9b2aa100320220ba0cb552d64
Elliot Cooke <elliotcooke@live.com>
2025-07-26 12:21:43 +0100

    Sololearn - debugging

---
+++ 2.0 - Sololearn/2.08 - Debugging.py
@@ -0,0 +1,34 @@
+""" Coding consists of 3 steps:
+
+- Writing
+
+- Executing (or running)
+
+- Fixing errors (or debugging)
+
+ 
+
+In this lesson, you’ll start step 3: identifying and fixing errors. """
+
+#paywall first thing...
+#print 5 + 3
+
+surname = "Smith"
+age = 32
+
+#paywall
+
+brand = "Audi"
+model = "Q5"
+print(model)
+
+""" Lesson Takeaways
+You did it! You learned that:
+
+ 
+
+🌟 Errors in code are known as bugs
+
+🌟 Code is executed line by line from top to bottom
+
+🌟 Code execution is interrupted by bugs """
