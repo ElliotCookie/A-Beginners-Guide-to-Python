33edd3b36b7198c92dc249ac8338f358bc4a9add
Elliot Cooke <elliotcooke@live.com>
2025-07-27 22:04:36 +0100

    Standards and best practise

---
+++ 2.0 - Sololearn/2.09 - Standards and best practises.py
@@ -0,0 +1,29 @@
+""" Millions of coders develop and maintain computer programs every day.
+Universal standards and best practices are needed for collaboration.
+In this lesson you’ll start applying some of these standards and best 
+practices used in the tech industry. """
+
+#Professional developers use comments to add descriptions and
+# explanations to their programs.
+
+credit = 300
+Credit = 280
+CREDIT = 320
+
+#this is new, case sensitive stuff
+
+""" Snake case is a popular way to create variable names in a consistent way.
+Snake case uses underscores _ to separate words in a variable name. """
+
+#Using snake case
+credit_score = 700
+
+"""
+Great work! You learned that:
+ 
+
+🌟 You can add comments to your code with the hash symbol #
+
+🌟 Python is a case-sensitive language
+
+🌟 Snake case is the best practice when creating multi-word variable names  """
