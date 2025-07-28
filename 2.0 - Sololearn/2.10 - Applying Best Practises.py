46078e319ddefdf2d70e9ed135446f70c7cdfc47
Elliot Cooke <elliotcooke@live.com>
2025-07-28 10:53:40 +0100

    Solo learn - applying best practise and renames

---
+++ 2.0 - Sololearn/2.10 - Applying Best Practises.py
@@ -0,0 +1,25 @@
+format="mp3"
+print(format)
+
+#A variable name can contain numbers but cannot start with a number.
+#This is a new one
+
+#looks like variables are lowercase with underscores
+salary = 900
+new_salary = salary + 200
+print(new_salary)
+
+payment_status = "returned"
+payment_status = "paid"
+print(payment_status)
+
+""" Lesson Takeaways
+You are unstoppable! You learned that:
+
+ 
+
+🌟 Spaces are not allowed in variable names
+
+🌟 A variable name cannot start with a number
+
+🌟 Best practices can help you avoid errors """
