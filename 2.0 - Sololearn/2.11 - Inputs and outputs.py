ed61bd35c03e8062d9f1f995aae6e0cb6574659c
Elliot Cooke <elliotcooke@live.com>
2025-07-29 22:16:31 +0100

    Input and output

---
+++ 2.0 - Sololearn/2.11 - Inputs and outputs.py
@@ -0,0 +1,36 @@
+""" Computer programs are designed to interact with users and the outside world.
+In this lesson, you'll learn to create code that
+ takes information in and sends information out. """
+
+#again, paywall early on
+
+#A robot vacuum cleaner makes a beeping sound when it's stuck.
+#  This sound is an example of…#
+#A robot vacuum cleaner is equipped with a sensor that helps it navigate
+#  its environment and detect obstacles.
+
+#The information coming from the sensor is an example of…
+
+#Select the correct way to name the variable using snake case style
+
+proximity_sensor = "obstacle"
+proximity_sensor = "clear"
+
+username = "zombie"
+points = 50
+lives = 3
+print(points)
+print(lives)
+
+address = input() #we've not really learnt this enough yet
+
+""" Lesson Takeaways
+You did it! You learned that:
+
+ 
+
+🌟 inputs and outputs help machines communicate with the outside world
+
+🌟 the input() instruction allows the user to enter a value into your program
+
+🌟 the print() instruction is used to generate an output """
