7b7b47801e5207ae5d0d87b8bbde4355b3d4bf8e
Elliot Cooke <elliotcooke@live.com>
2025-07-30 21:50:07 +0100

    Data types

---
+++ 2.0 - Sololearn/2.12 - Data types.py
@@ -0,0 +1,32 @@
+""" Data comes in different shapes and forms. 
+Computers treat different types of data in different ways.
+In this lesson, you'll start working with different types of data. """
+
+#String is the data type for a piece of text. 
+# #The quotation marks tell the computer that 
+# a value needs to bestored as a string.
+
+variable = 5/2 #will store as a float
+
+string = "accepted"
+integer = 5
+float = 2.5
+
+a = "basket"
+b = "ball"
+print(a+b) # concatenation
+
+print("360" + "360")
+
+a = "foot"
+b = "ball"
+print(a + b)
+
+""" Lesson Takeaways
+Great job! You learned that:
+
+🌟 computers store and process different data types differently
+
+🌟 string is the data type for text
+
+🌟 integer and float are data types for numbers """
