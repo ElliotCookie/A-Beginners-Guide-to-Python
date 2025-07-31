99d635744ad4193ad2a75344b2fba9450362f1df
Elliot Cooke <elliotcooke@live.com>
2025-07-31 19:41:20 +0100

    Module quiz

---
+++ 2.0 - Sololearn/2.13 - Module quiz.py
@@ -0,0 +1,20 @@
+""" It’s time to put your skills to the test.
+The end-of-module quiz is your chance to apply what you’ve learned,
+reinforce your knowledge, and see how far you’ve come.
+Ready to take the final step? """
+
+message = "Debugging
+print(message)
+
+#find the bug
+band = "Beatles"
+song = Yesterday
+year = 1965
+#so python actually just tells you, that's nice
+
+#I actually just got my first one wrong!
+
+c = "moon"
+b = "guide"
+a = "honey"
+print(a + c) # CRAFTY!
