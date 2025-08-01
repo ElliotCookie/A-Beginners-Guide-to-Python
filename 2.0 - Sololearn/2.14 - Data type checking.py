87a72bbc3630ccb55e294283a120fe8a58607b10
Elliot Cooke <elliotcooke@live.com>
2025-08-01 12:12:23 +0100

    Data stuff

---
+++ 2.0 - Sololearn/2.14 - Data type checking.py
@@ -0,0 +1,29 @@
+#guess the output
+a = "note"
+b = "book"
+print(a+b)
+
+balance = "780"
+type(balance)
+
+#2nd paywall there
+
+budget = 200
+expenses = "180"
+#savings = budget - expenses
+
+
+x = 9
+y = 3
+z = x / y
+print(type(z)) # I didn't know this one was a float!
+
+
+""" Lesson Takeaways
+Great job! You learned that:
+
+ 
+
+🌟 the type() instruction is used to check the data type
+
+🌟 the division of two integers always produces a float """
---
+++ 2.0 - Sololearn/2.15 - Data conversion.py
@@ -0,0 +1,26 @@
+""" Data can come in the incorrect format. 
+Data from surveys and web forms can come to you with quality issues.
+In this lesson, you'll learn to convert data to get it into the correct format. """
+
+a = "14"
+b = int(a)
+c = input() # this will be a string
+
+""" In a similar way, you can ensure that values are converted
+into strings with the str() instruction.
+Complete the code to convert the value into a string
+ """
+a = 2.9
+b =str(a)
+
+
+""" Lesson Takeaways
+Great job! You learned that:
+
+ 
+
+🌟 you can change the data type of a value with int(), float() and str()
+
+🌟 there are implicit and explicit data type conversions in Python
+
+🌟 str(), int(), float() instructions are explicit conversions """
