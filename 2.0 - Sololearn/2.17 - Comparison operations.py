822cadb421b1faec2ec4225317b74cd6054de4c6
Elliot Cooke <elliotcooke@live.com>
2025-08-03 21:22:59 +0100

    Comparison operations

---
+++ 2.0 - Sololearn/2.17 - Comparison operations.py
@@ -0,0 +1,24 @@
+""" Computers are faster and more precise than humans at certain operations.
+In this lesson, you'll learn about a type of operation that makes 
+machines evaluate different scenarios and make decisions. """
+
+if 5 < 9:
+    print("5 is more than 9")
+
+print(30 < 25)
+#paywall
+
+#this looks hard
+""" Electronic circuits inside computers use millions
+of tiny switches to store these True/False values.
+How many possible positions do these switches have? """
+
+""" Binary code is made up of 0s and 1s and the tiny switches allow #
+computers to perform billions of tiny operations very… """
+
+#Finally, the soil moisture thing
+
+""" Lesson Takeaways
+Amazing! You learned that:
+🌟 the Boolean data type has one of two possible values: True or False
+🌟 a comparison operation always results in a Boolean """
