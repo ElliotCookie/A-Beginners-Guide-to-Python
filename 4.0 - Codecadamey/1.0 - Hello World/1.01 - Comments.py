d4aea081c9a07dc6fd6407ed87b866366729dd20
Elliot Cooke <elliotcooke@live.com>
2025-10-07 19:35:17 +0100

    New day, new hello world

---
+++ 4.0 - Codecadamey/1.0 - Hello World/1.01 - Comments.py
@@ -0,0 +1,11 @@
+#you use the symbol to comment
+#I forget how basic this starts
+
+print("hello world")
+
+#advert
+
+#interestingly the print syntax in Python 2 vs Python 3 is different
+#print "hello world" <- no brackets
+
+
