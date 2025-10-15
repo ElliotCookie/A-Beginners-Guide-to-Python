ab0ca7bbc0eaa111047e312b36c755e3c9a40d0c
Elliot Cooke <elliotcooke@live.com>
2025-10-15 18:34:30 +0100

    Some string formatting

--- 4.0 - Codecadamey/1.0 - Hello World/1.03 - Tip calculator.py
+++ 4.0 - Codecadamey/1.0 - Hello World/1.03 - Tip calculator.py
@@ -9,3 +9,32 @@ total = meal + meal * tip
 print(total)
 
 #short 6 q test
+
+#Strings & Console output
+brian = "Hello life!"
+
+caesar = "Graham"
+praline = "John"
+viking = "Teresa"   
+print (caesar)
+print (praline)
+print (viking)
+
+#fix this: 'This isn't flying, this is falling with style!'
+#you can use the backslash to allow ''s 
+
+string = ("This isn't flying, this is falling with style!")
+print(string)
+#this works in Py 3, but not 2!
+
+montyName = "MONTY"
+fifth_letter = montyName[4] #make sure to use [] and not ()
+print (fifth_letter)
+
+parrot = "Norwegian Blue"
+print (len(parrot))
+print (parrot.lower())
+print (parrot.upper())
+
+pi = 3.14
+print (str(pi))
