8ec7d0ecad81cc51d839095a0a54e7a7ac3af002
Elliot Cooke <elliotcooke@live.com>
2025-10-17 15:09:05 +0100

    Finished another lesson

---
+++ 4.0 - Codecadamey/1.0 - Hello World/1.04 - Date and time.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+#This is awsome: you get 6dp on the seconds
+now = datetime.now()
+print (now)
+
+current_year = now.year
+current_month = now.month
+current_day = now.day
+
+print(now.year)
+print(now.month)
+print(now.day)
+
+print ("%02d/%02d/%04d" % (now.month, now.day, now.year))
+
+print (now.hour)
+print (now.minute)
+print (now.second)
+print ('%02d:%02d:%04d' % (now.hour, now.minute, now.second))
+
+print ("%02d/%02d/%04d %02d:%02d:%02d" % (now.month, now.day, now.year, now.hour, now.minute, now.second))
