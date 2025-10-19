8daf1db90b7ae676b82b099a4bb757ed2be12bd0
Elliot Cooke <elliotcooke@live.com>
2025-10-19 21:12:38 +0100

    Website update

--- 4.0 - Codecadamey/1.0 - Hello World/1.04 - Date and time.py
+++ 4.0 - Codecadamey/1.0 - Hello World/1.04 - Date and time.py
@@ -19,3 +19,5 @@ print (now.second)
 print ('%02d:%02d:%04d' % (now.hour, now.minute, now.second))
 
 print ("%02d/%02d/%04d %02d:%02d:%02d" % (now.month, now.day, now.year, now.hour, now.minute, now.second))
+
+#not forgotten, just doing website stuff today
