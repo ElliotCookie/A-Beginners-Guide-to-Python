935f5ffed6656506722290fb0865136d523e6bdf
Elliot Cooke <elliotcooke@live.com>
2025-10-28 07:51:06 +0000

    progress with pig latin

---
+++ 4.0 - Codecadamey/1.0 - Hello World/1.07 - PygLatin.py
@@ -0,0 +1,19 @@
+print("Pig Latin")
+pyg = "ay"
+original = input("Enter a word: ")
+
+if len(original) > 0:
+    print(f"Valid string: {original}")
+else:
+    print("empty")
+
+
+if len(original) > 0 and original.isalpha() == True: #don't forget ()
+    word = original.lower()
+    print("Valid string: %s" % (word))
+    first = word[0]
+    new_word = word + first + pyg
+else:
+    print("empty or non-alpha")
+
+
