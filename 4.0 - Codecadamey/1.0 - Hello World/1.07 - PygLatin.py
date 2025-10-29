27c3dd07421637720cda5f509e2badece61e92d2
Elliot Cooke <elliotcooke@live.com>
2025-10-29 12:18:02 +0000

    Progress on pyglatin

--- 4.0 - Codecadamey/1.0 - Hello World/1.07 - PygLatin.py
+++ 4.0 - Codecadamey/1.0 - Hello World/1.07 - PygLatin.py
@@ -13,6 +13,7 @@ if len(original) > 0 and original.isalpha() == True: #don't forget ()
     print("Valid string: %s" % (word))
     first = word[0]
     new_word = word + first + pyg
+    new_word = new_word[1:len(new_word)]
 else:
     print("empty or non-alpha")
 
