5a38cc140782a1bdc2c33e45c9e3591b405c92f7
Elliot Cooke <elliotcooke@live.com>
2025-11-11 20:20:23 +0000

    Snycing 1.9 - functions

--- 4.0 - Codecadamey/1.0 - Hello World/1.08 - Functions.py
+++ 4.0 - Codecadamey/1.0 - Hello World/1.08 - Functions.py
@@ -15,4 +15,30 @@ base_input = int(input("base: "))
 power_input = int(input("power: "))
 power(base_input, power_input) 
 
-#looking at making an etf creator, no progress!
+#Functions calling functions
+def one_good_turn(n):
+  return n + 1
+    
+def deserves_another(n):
+  return one_good_turn(n) + 2
+
+def shout(phrase):
+  if phrase == phrase.upper():
+    return "YOU'RE SHOUTING!"
+  else:
+    return "Can you speak up?"
+
+shout("I'M INTERESTED IN SHOUTING")
+
+def cube(number):
+  return number*number*number
+
+print(cube(1))
+
+def by_three(number):
+  if number % 3 == 0:
+    return cube(number)
+  else:
+    return False
+  
+root = sqrt(25)
