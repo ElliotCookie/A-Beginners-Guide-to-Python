21bfe3079ed904c2731b256cb58a34af7c4f0e8e
Elliot Cooke <elliotcooke@live.com>
2025-11-28 10:01:42 +0000

    reducing line count

--- 4.0 - Codecadamey/1.0 - Hello World/1.11 - The student becomes the teacher.py
+++ 4.0 - Codecadamey/1.0 - Hello World/1.11 - The student becomes the teacher.py
@@ -1,14 +1,14 @@
 lloyd = {}
 alice = {}
 tyler = {}
+dict_list = [lloyd, alice, tyler]
 
 newkeys = ["name", "homework", "quizzes", "tests"]
 
 def adding_keys_to_dicts(keylist):
     for keys in keylist:
-        lloyd[keys] = []
-        alice[keys] = []
-        tyler[keys] = []
+        for dicts in dict_list:
+            dicts[keys] = []
     
 adding_keys_to_dicts(newkeys)
 
@@ -16,6 +16,5 @@ lloyd["name"] = "Lloyd"
 alice["name"] = "Alice"
 tyler["name"] = "Tyler"
 
-print(lloyd)
-print(alice)
-print(tyler)
+for dict in dict_list:
+    print(dict)
