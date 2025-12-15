e98647357ee3cac07451eaaf32b4186c369610fc
Elliot Cooke <elliotcooke@live.com>
2025-12-15 11:20:01 +0000

    furthering battleship

--- 4.0 - Codecadamey/1.0 - Hello World/1.13 - Battleships.py
+++ 4.0 - Codecadamey/1.0 - Hello World/1.13 - Battleships.py
@@ -6,7 +6,7 @@ for coordinates in range(5):
   board.append(['O'] * 5)
 #print(board)
 
-def print_board(board_in):
+def print_board(board):
   for rows in board:
     #print(rows)
     print(" ".join(rows))
@@ -35,3 +35,24 @@ def random_row(board_in):
   
 def random_col(board_in):
   return randint(0, len(board_in) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+print(ship_row)
+print(ship_col)
+
+guess_row = int(input("Guess Row: "))
+guess_col = int(input("Guess Col: "))
+
+if (guess_row == ship_row) & (guess_col == guess_col):
+  print("Congratulations! You sank my battleship!") 
+else:
+  print("You missed my battleship!")
+  #find board row
+  #go to location in row
+  #set value there to X
+  attacked_row = board[guess_row-1]
+  attacked_row[guess_col-1] = "X"
+  print_board(board)
+  # It's saying it doesn't work, but it does?!
+  #they want:1  board[guess_row][guess_col] = "X"
