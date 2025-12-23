be1e7dcd8e970fa208723cad2e92ea9936ad8982
Elliot Cooke <elliotcooke@live.com>
2025-12-23 13:54:23 +0000

    While loops, with an else

--- 4.0 - Codecadamey/1.0 - Hello World/1.14 - Loops.py
+++ 4.0 - Codecadamey/1.0 - Hello World/1.14 - Loops.py
@@ -39,3 +39,46 @@ while count < 10:
   print (count)
   count += 5
   # Increment count - although you mean by one, but you it didn't mention this
+
+
+  #Using breaks
+count = 0
+
+while True:
+  print (count)
+  count += 1
+  if count >= 10:
+    break
+
+import random
+
+print ("Lucky Numbers! 3 numbers will be generated.")
+print ("If one of them is a '5', you lose!")
+
+count = 0
+while count < 3:
+  num = random.randint(1, 6)
+  print (num)
+  if num == 5:
+    print ("Sorry, you lose!")
+    break
+  count += 1
+else:
+  print ("You win!")
+
+
+#else within while, my turn
+from random import randint
+
+# Generates a number from 1 through 10 inclusive
+random_number = randint(1, 10)
+
+guesses_left = 3
+while guesses_left > 0:
+  guess = input("Please guess")
+  if guess == random_number:
+    print("You win!")
+    break
+  guesses_left -= 1
+else:
+  print("You lose!")
