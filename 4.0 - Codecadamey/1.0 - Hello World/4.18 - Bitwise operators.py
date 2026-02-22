480a393fe8926a904a4f5d3bf5bcc6f7c1ec5ae2
Elliot Cooke <elliotcooke@live.com>
2026-02-22 13:53:55 +0000

    Bitwise operators

---
+++ 4.0 - Codecadamey/1.0 - Hello World/4.18 - Bitwise operators.py
@@ -0,0 +1,21 @@
+# 1 / 14 - Bitwise operators
+print (5 >> 4)  # Right Shift       0
+print (5 << 1  )# Left Shift        10
+print (8 & 5 )  # Bitwise AND       0
+print (9 | 4)   # Bitwise OR        13
+print (12 ^ 42 )# Bitwise XOR       38
+print (~88  )   # Bitwise NOT       -89
+
+
+# 2 / 14 - The 2 base number system
+# This is essentially binary, 0b is the protected keyword that lets you interact this way
+print (0b1,)   #1
+print (0b10,)  #2
+print (0b11,)   #3
+print (0b100,)  #4
+print (0b101,)  #5
+print (0b110,)  #6
+print (0b111 )  #7
+print ("******")
+print (0b1 + 0b11)
+print (0b11 * 0b11)
