77491e5e5e754720e72aff29675dc38cf4a1a042
Elliot Cooke <elliotcooke@live.com>
2026-02-24 08:37:53 +0000

    Int has a second parameter?!

--- 4.0 - Codecadamey/1.0 - Hello World/4.18 - Bitwise operators.py
+++ 4.0 - Codecadamey/1.0 - Hello World/4.18 - Bitwise operators.py
@@ -41,3 +41,17 @@ print(bin(1)) # 0b1
 
 for nos in range(2,6):
     print(bin(nos))
+
+
+
+
+# 5 / 14 - There is a second parameter on the int function!
+#When given a string containing a number and the base that number is in, the function will return
+# the value of that number converted to base ten.
+print("Second parameter of int...")
+print (int("1",2))
+print (int("10",2))
+print (int("111",2))
+print (int("0b100",2))
+print (int(bin(5),2))
+print(int("11001001", 2))
