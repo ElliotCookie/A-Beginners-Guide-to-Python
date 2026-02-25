da4455d53c117a281c1ffa874c866a88490158fd
Elliot Cooke <elliotcooke@live.com>
2026-02-25 07:54:52 +0000

    Shifting bits

--- 4.0 - Codecadamey/1.0 - Hello World/4.18 - Bitwise operators.py
+++ 4.0 - Codecadamey/1.0 - Hello World/4.18 - Bitwise operators.py
@@ -55,3 +55,34 @@ print (int("111",2))
 print (int("0b100",2))
 print (int(bin(5),2))
 print(int("11001001", 2))
+
+
+
+
+
+
+# 6 / 14 - Shifting bits left and right
+print("shifting bits...")
+""" Shift operations are similar to rounding down after dividing
+and multiplying by 2 (respectively) for every time you shift, 
+but it’s often easier just to think of it as shifting all the 1s and 0s
+left or right by the specified number of slots. """
+
+# Left Bit Shift (<<)  
+0b000001 << 2 == 0b000100 #(1 << 2 = 4)
+0b000101 << 3 == 0b101000 #(5 << 3 = 40)       
+
+# Right Bit Shift (>>)
+0b0010100 >> 3 == 0b000010 #(20 >> 3 = 2)
+0b0000010 >> 2 == 0b000000 #(2 >> 2 = 0) 
+
+
+shift_right = 0b1100
+shift_left = 0b1
+
+# Your code here!
+shift_left << 2 == 0b100
+shift_right >> 2 == 0b11 
+
+print (bin(shift_right))
+print (bin(shift_left))
