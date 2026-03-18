406c540495c608d4e89a58546122ff34fe217554
Elliot Cooke <elliotcooke@live.com>
2026-03-18 07:14:54 +0000

    Shopping cart class

--- 4.0 - Codecadamey/1.0 - Hello World/4.19 - Introduction to classes.py
+++ 4.0 - Codecadamey/1.0 - Hello World/4.19 - Introduction to classes.py
@@ -243,3 +243,39 @@ print(sloth.health)
 print(ocelot.health)
 
 # rejected over my solution as I spelt Good with a G, not a g
+
+
+
+
+# 10 / 18 - Modelling real world objects
+
+print("Shopping cart example...")
+
+class ShoppingCart(object):
+  """Creates shopping cart objects
+  for users of our fine website."""
+  
+  def __init__(self, customer_name):
+    self.customer_name = customer_name
+    self.items_in_cart = {}
+  def add_item(self, product, price):
+    """Add product to the cart."""
+    if not product in self.items_in_cart:
+      self.items_in_cart[product] = price
+      print (product + " added.")
+    else:
+      print (product + " is already in the cart.")
+
+  def remove_item(self, product):
+    """Remove product from the cart."""
+    if product in self.items_in_cart:
+      del self.items_in_cart[product]
+      print (product + " removed.")
+    else:
+      print (product + " is not in the cart.")
+
+my_cart = ShoppingCart("Elliot")
+my_cart.add_item("Bananas", 2) # I think JSON or react guards against this potential error happening - the dots are aligning!!!
+
+
+
