322d626af5a7ca2200c0ba975fce7b30ea8ad235
Elliot Cooke <elliotcooke@live.com>
2026-03-19 08:19:26 +0000

    Inhertiance

--- 4.0 - Codecadamey/1.0 - Hello World/4.19 - Introduction to classes.py
+++ 4.0 - Codecadamey/1.0 - Hello World/4.19 - Introduction to classes.py
@@ -279,3 +279,27 @@ my_cart.add_item("Bananas", 2) # I think JSON or react guards against this poten
 
 
 
+
+# 11 / 18 - Starting inheritance
+
+# Where a class takes on the ayyributes and methods of another
+# used to express an is-a relationship
+# This is to say a bear class or a bird class could inherit from an animal class, but not from each other
+
+
+class Customer(object):
+  """Produces objects that represent customers."""
+  def __init__(self, customer_id):
+    self.customer_id = customer_id
+
+  def display_cart(self):
+    print ("I'm a string that stands in for the contents of your shopping cart!")
+
+class ReturningCustomer(Customer):
+  """For customers of the repeat variety."""
+  def display_order_history(self):
+    print ("I'm a string that stands in for your order history!")
+
+monty_python = ReturningCustomer("ID: 12345")
+monty_python.display_cart()
+monty_python.display_order_history()
