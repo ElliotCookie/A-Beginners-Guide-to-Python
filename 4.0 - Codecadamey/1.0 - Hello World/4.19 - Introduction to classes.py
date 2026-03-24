0f64c461234035ed6a19628b52bf8047b3498cb4
Elliot Cooke <elliotcooke@live.com>
2026-03-24 08:09:54 +0000

    Making a triangle class

--- 4.0 - Codecadamey/1.0 - Hello World/4.19 - Introduction to classes.py
+++ 4.0 - Codecadamey/1.0 - Hello World/4.19 - Introduction to classes.py
@@ -451,7 +451,24 @@ my_triangle = Triangle(90, 30, 60)
 print(my_triangle.number_of_sides)
 #print(my_triangle.check_angles())
 #print(my_triangle.self_check_angles(my_triangle))
-print(my_triangle.self2_check_angles())
+print(my_triangle.self2_check_angles()) # this is actually what they wanted
 
 # okay, I think we learned something there
 # by parsing self, you give the function acess to all the vars in that instance
+
+
+
+# 18 / 18 - Creating an instance
+class Equilateral(Triangle):
+  angle = 60
+  def __init__(self, angle1, angle2, angle3):
+    super().__init__(angle1, angle2, angle3) #this must match the parent class
+    self.angle1 = self.angle
+    self.angle2 = self.angle
+    self.angle3 = self.angle
+
+
+
+
+e = Equilateral(0,0,0)
+print(e.angle1) #60
