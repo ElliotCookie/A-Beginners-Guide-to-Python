9c38fab7d7e71e31374d05be0474b14c578cbb2a
Elliot Cooke <elliotcooke@live.com>
2026-03-26 07:41:10 +0000

    making instance variables again

--- 4.0 - Codecadamey/1.0 - Hello World/4.20 - Class basics.py
+++ 4.0 - Codecadamey/1.0 - Hello World/4.20 - Class basics.py
@@ -1,8 +1,12 @@
 # 1 / 11 - Class basics (did we not just sort of do this??)
 class Car(): # don't forget (object) in py 2
     condition = "new"
+    def __init__(self, model, colour, mpg):
+        self.model = model
+        self.colour = colour # obviously the solution was rejected for this
+        self.mpg = mpg
 
-my_car = Car()
+my_car = Car("DeLorean", "silver", 88)
 
 # 2/ 11 - creating an instance of the object
 
@@ -10,3 +14,14 @@ my_car = Car()
 
 # 4 / 11 - printing member var
 print(my_car.condition) # new
+
+# 5 / 11 - Initialising a class
+# do they realise we've just done this? The explanations are like it's the first we are hearing of it
+
+# to assign a variable to the class (creating a member variable)
+# use dot notation
+# but then the followup example mismatches new_variable and newVariable which isn't useful
+print("Checking instance variables have worked...")
+print(my_car.mpg)
+
+# solution rejected for using colour over color
