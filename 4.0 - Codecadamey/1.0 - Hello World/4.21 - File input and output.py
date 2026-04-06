2b16e62a22597cc7f01d28d36eb1246334f97335
Elliot Cooke <elliotcooke@live.com>
2026-04-06 09:02:18 +0100

    Reading and writing

--- 4.0 - Codecadamey/1.0 - Hello World/4.21 - File input and output.py
+++ 4.0 - Codecadamey/1.0 - Hello World/4.21 - File input and output.py
@@ -16,7 +16,36 @@ f.close()
 
 
 
+
+
 # 2 / 9 - The open function
 # basically the process of identifying a file and setting it up to be interacted with
 
 my_file = open("output.txt", "r+") # the second argument allows you to do different things ofc
+
+
+
+
+
+# 3 / 9 - Writing
+# the write() function takes a string argument
+# you also need to remember to close the file, or "python won't write to it properly"
+
+# Instructions aren't that clear on this one, considering it is a new lesson
+
+for item in my_list:
+  my_file.write(str(item)+ "\n") # kinda cheating as they already gave us this
+my_file.close()  
+
+
+
+
+
+
+# 4 / 9 - Reading
+# I have a feeling this might be similar to writing, although we won't need to offer up a str() ofc
+
+my_new_file = open("output.txt", "r")
+print(my_new_file.read()) # the .read() goes outside the object, not read(object)
+# DON'T FORGET TO CLOSE
+my_new_file.close()
