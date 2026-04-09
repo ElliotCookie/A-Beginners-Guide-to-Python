deeeadb6f9afe881d00ff60f0319b6fadde9f446
Elliot Cooke <elliotcooke@live.com>
2026-04-09 17:43:03 +0100

    Still going

--- 4.0 - Codecadamey/1.0 - Hello World/4.21 - File input and output.py
+++ 4.0 - Codecadamey/1.0 - Hello World/4.21 - File input and output.py
@@ -132,4 +132,6 @@ print("Join into a string finished")
 
 # I think we need to unpack how all of these work tbh, readline is cool though!
 # I've been doing css, so that sort of counts as progress but not here 
+# Again in the morning, even using inspector on chrome!
+
 
